524f967c662af1480a4bcb8c70d60335e3c7d44d
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-16 23:04:15 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -4,3 +4,4 @@ sudo apt install python3-pip
 python3 -m pip install pytz
 pip install -r https://raw.githubusercontent.com/XyHK-HUC/Serv00-Reg/main/requirements.txt
 python3 -m pip install --upgrade requests
+python3 main.py
