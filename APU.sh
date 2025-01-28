09cf81abf412bb9e3507decef4693c45ba426a62
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-28 15:11:08 +0800

    Update APU.sh

--- APU.sh
+++ APU.sh
@@ -7,11 +7,11 @@ run_script() {
     case "$os_name" in
         debian|ubuntu)
             echo "检测到系统: $os_name"
-            apt update && apt install bash  &&  apt install curl -y && bash <(curl -fsSL https://cfgithub.gw2333.workers.dev/https://github.com/mi1314cat/One-click-script/raw/refs/heads/main/Ubuntu.sh)
+            bash <(curl -fsSL "$SCRIPT_A")
             ;;
         alpine)
             echo "检测到系统: $os_name"
-            apt update && apt install bash  &&  apt install curl -y && bash <(curl -fsSL https://cfgithub.gw2333.workers.dev/https://github.com/mi1314cat/One-click-script/raw/refs/heads/main/Ubuntu.sh)
+            bash <(curl -fsSL "$SCRIPT_B")
             ;;
         *)
             echo "不支持的系统: $os_name。此脚本不支持当前系统，程序退出。"
@@ -20,7 +20,9 @@ run_script() {
     esac
 }
 
-
+# 定义脚本URL
+SCRIPT_A="https://cfgithub.gw2333.workers.dev/https://github.com/mi1314cat/One-click-script/raw/refs/heads/main/Ubuntu.sh"
+SCRIPT_B="https://cfgithub.gw2333.workers.dev/https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/alpine.sh"
 
 # 调用函数
 run_script
