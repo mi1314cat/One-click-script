66eb2c2e243cfe1a6965a6b27ed594ef1f95ab5e
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2026-04-17 20:03:48 +0800

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -61,7 +61,7 @@ main_menu() {
 initialize_dependencies() {
     echo "检查并安装基础依赖..."
     apt update 
-    apt upgrade
+    apt upgrade -y
     apt install ufw -y
     apt install -y curl socat git cron openssl gzip nano sudo wget
   
